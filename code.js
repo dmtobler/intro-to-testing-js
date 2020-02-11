478256b9dcfbcc24ede30b8c2a1226613e8f5e1f
Danielle Tobler <danielle.tobler210@gmail.com>
2020-02-10 18:24:58 -0600

    Completed implementation and unit tests for isVowel function

--- code.js
+++ code.js
@@ -35,3 +35,18 @@ function isEven(input) {
         return input % 2 === 0;
     }
 }
+
+// isVowel function
+
+function isVowel(input) {
+    switch (typeof input === "string" && input.toUpperCase()) {
+        case "A":
+        case "E":
+        case "I":
+        case "O":
+        case "U":
+            return true;
+        default:
+            return false;
+    }
+}
